664268ae8b85b111c433e85965e4b054950285f9
agrawalgaurav <gaurav.agrawal@huawei.com>
2020-05-14 00:33:01 +0530

    Test artifacts for applcm.
    Change-Id: I9840ac5fc6f91bfc0b4ce6abbc790a158f6b2d49

---
+++ mecm/mepm/applcm/resources/Build-Run.sh
@@ -0,0 +1,9 @@
+AppLcm broker compile and build docker image 
+/applcm/broker/docker-build.sh
+
+
+helmplugin compile and build docker image 
+/applcm/k8shelm/docker-build.sh
+
+Run docker images:
+run docker-compose file
---
+++ mecm/mepm/applcm/resources/README.md
@@ -0,0 +1,2 @@
+This folder contains test artifacts to try out applcm in local machine
+Note: For production k8s based deployment will be used, this folder is just for local testing.
