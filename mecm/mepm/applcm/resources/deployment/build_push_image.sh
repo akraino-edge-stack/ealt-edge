2bb9c251cb17db7849a0a364fbe557175b336402
agrawalgaurav <gaurav.agrawal@huawei.com>
2020-05-15 20:35:36 +0530

    Image build and push scripts
    Change-Id: I8b3525e61c083d2fa82195ad5e08e72ba58301a7

--- mecm/mepm/applcm/resources/deployment/build_push_image.sh
+++ mecm/mepm/applcm/resources/deployment/build_push_image.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright 2020 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,5 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-MEP_VERSION=latest
-docker build --no-cache -t ealtedge/helmplugin:${MEP_VERSION} -f build/Dockerfile .
+CURRENT_DIR=`pwd`
+
+echo "DOCKER_BUILD_DIR=${CURRENT_DIR}"
+echo "Build and Push APP LCM"
+
+# Build and push broker
+cd ${CURRENT_DIR}
+cd ../../broker/
+. build_push_image.sh
+
+# Build and push k8s helm plugin
+cd ${CURRENT_DIR}
+cd ../../k8shelm/
+. build_push_image.sh
+
