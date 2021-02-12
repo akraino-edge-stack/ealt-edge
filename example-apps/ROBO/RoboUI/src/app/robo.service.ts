15d175baed7882c59d55c01e7aa373768adf6d21
Srinivasan <srinivasan.s.n@huawei.com>
2021-02-12 21:21:33 +0530

    image part baseurl modifications
    Signed-off-by: Srinivasan <srinivasan.s.n@huawei.com>
    Change-Id: I60cc613f0bd5897171144bbff44c55d48ac749c7

--- example-apps/ROBO/RoboUI/src/app/robo.service.ts
+++ example-apps/ROBO/RoboUI/src/app/robo.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/comm
 import { Observable,throwError } from 'rxjs'
 import { timer, Subscription, pipe } from 'rxjs';
 
-import { cameraData, camerainfo, cameraDetails, monitorDetails, monitorinfo, cameraID } from './datainterface'
+import { cameraData, camerainfo, cameraDetails, monitorDetails, monitorinfo, cameraID, appsPvcs, backupRestore, monitorImage } from './datainterface'
 
 
 @Injectable({
@@ -12,18 +12,33 @@ import { cameraData, camerainfo, cameraDetails, monitorDetails, monitorinfo, cam
 })
 export class RoboService {
 
-  private baseUrl = 'http://localhost:9996';
+  private baseUrl = 'http://159.138.33.54:30091';
 
-  private postCameraDetailsUrl = this.baseUrl + '/v1/monitor/cameras'
-  private cameraDetailsUrl = this.baseUrl + '/v1/monitor/cameras'
-  private cameraDetails_url = './../assets/data/camera.json'
+  private inventoryBaseUrl = 'http://159.138.33.54:30092';
 
-  private monitorDetails_url = './../assets/data/inventory.json'
-  private monitorDetailsUrl = this.baseUrl + '/v1/inventry/table'
+  private postCameraDetailsUrl = this.inventoryBaseUrl + '/v1/monitor/cameras' // 92
+  private cameraDetailsUrl = this.inventoryBaseUrl + '/v1/monitor/cameras' //
+  //private cameraDetails_url = './../assets/data/camera.json'
 
-  private monitorImageUrl = this.baseUrl + '/v1/monitor/image'
+  //private monitorDetails_url = './../assets/data/inventory.json'
+  private monitorDetailsUrl = this.inventoryBaseUrl + '/v1/inventry/table'
 
-  private triggerObjUrl = this.baseUrl + '/v1/monitor/cameras/'
+  private monitorImageUrl = this.inventoryBaseUrl + '/v1/inventry/image'
+
+  private triggerObjUrl = this.inventoryBaseUrl + '/v1/monitor/cameras/'
+
+  private appsPvcsDetailsUrl = this.baseUrl + '/v1/robo/apps-pvcs'
+  //private appsPvcsDetails_url = './../assets/data/appspvc.json'
+
+  private backupRestoreDetailsUrl = this.baseUrl + '/v1/robo/backup-restore'
+  
+  //private backupRestoreDetails_url = './../assets/data/backuprestore.json'
+
+  private postBackupDetailsUrl = this.baseUrl + '/v1/robo/backup'
+
+  private postRestoreDetailsUrl = this.baseUrl + '/v1/robo/restore'
+
+  private disasterUrl = this.baseUrl + '/v1/robo/disaster'
 
   constructor(private http:HttpClient) { }
   
@@ -55,9 +70,9 @@ export class RoboService {
     return this.http.get<monitorDetails>(this.monitorDetailsUrl);
   }
 
-  getMonitorImage(): Observable<Blob> {
+  getMonitorImage(): Observable<any> {
     debugger;
-    return this.http.get<Blob>(this.monitorImageUrl);
+    return this.http.get<any>(this.monitorImageUrl);
   }
 
 
@@ -66,5 +81,33 @@ export class RoboService {
     debugger;
     this.triggerObjUrl = this.triggerObjUrl + data;
     return this.http.get<any>(this.triggerObjUrl)
+  }
+
+  getAppsPvcsInfo(): Observable<appsPvcs> {
+    return this.http.get<appsPvcs>(this.appsPvcsDetailsUrl);
+  }
+
+  // getBackupRestoreInfo(): Observable<backupRestore> {
+    // }
+    //   return this.http.get<backupRestore>(this.backupRestoreDetails_url);
+
+  getBackupRestoreInfo(): Observable<any> {
+    return this.http.get<any>(this.backupRestoreDetailsUrl);
+  }
+
+  disturbCluster(): Observable<any> {
+    return this.http.get<any>(this.disasterUrl);
+  }
+
+  postBackup(data): Observable<any> {
+    console.log(data);
+    debugger;
+    return this.http.post<any>(this.postBackupDetailsUrl, data)
+  }
+
+  postRestore(data): Observable<any> {
+    console.log(data);
+    debugger;
+    return this.http.post<any>(this.postRestoreDetailsUrl, data)
   }
 }
